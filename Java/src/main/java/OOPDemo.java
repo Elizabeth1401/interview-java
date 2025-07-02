c68c40edcc8602c3b344ce2c25f16bcacfc83b2e
Elizabeth1401 <140805762+Elizabeth1401@users.noreply.github.com>
2025-07-02 15:40:16 +0200

    code for demonstration OOP Java and Unit Test

---
+++ Java/src/main/java/OOPDemo.java
@@ -0,0 +1,11 @@
+public class OOPDemo
+{
+  public static void main(String[] args)
+  {
+    Animal dog = new Dog("Buddy");
+    Animal cat = new Cat("Whiskers");
+
+    System.out.println(dog.speak());
+    System.out.println(cat.speak());
+  }
+}
---
+++ Java/src/test/java/AnimalTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnimalTest
+{
+  @Test
+  public void testDogSpeak() {
+    Dog dog = new Dog("Buddy");
+    assertEquals("Buddy says: Woof", dog.speak());
+  }
+
+  @Test
+  public void testCatSpeak() {
+    Cat cat = new Cat("Whiskers");
+    assertEquals("Whiskers says: Meow", cat.speak());
+  }
+
+}
